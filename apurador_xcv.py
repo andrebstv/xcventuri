59d70d6af31912a29cfd2d1c48234276b2026566
Andre <andrebstv@gmail.com>
2023-04-08 13:14:00 -0300

    Buscando voos e organizando em base de dados.

--- apurador_xcv.py
+++ apurador_xcv.py
@@ -2,7 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import pandas as pd
+import sqlite3
 from datetime import datetime as dt
+from unidecode import unidecode
+from unicodedata import normalize
+
 
 #Fazer a lista de pilotos e links e interar.
 
@@ -10,28 +14,75 @@ from datetime import datetime as dt
 df_pilotos_ids = pd.read_csv("ids_base_pilotos.csv", sep=';')
 #formato da apuração
 df_apuracao = pd.read_csv("formato_da_apuracao.csv", sep=';')
-
+voos_validos = pd.DataFrame()
 #loop completo
 for i in range(len(df_pilotos_ids)):
     piloto = df_pilotos_ids.loc[i].at['Nome']
+    vela = df_pilotos_ids.loc[i].at['Vela']
     id_ativo = df_pilotos_ids.loc[i].at['ID_XCBrasil']
+    categoria = df_pilotos_ids.loc[i].at['Categoria']
     data_limite = dt.strptime(df_pilotos_ids.loc[i].at['Data_Entrada'], "%d/%m/%Y")
+    
     html = requests.get("http://xcbrasil.com.br/tracks/world/alltimes/brand:all,cat:0,class:all,xctype:all,club:all,pilot:"+ id_ativo +",takeoff:all&sortOrder=DATE").content
+    #html = unidecode(html)
     soup = BeautifulSoup(html, 'html.parser')
     site_limpo = soup.prettify()
-    site_limpo.encode(encoding = 'UTF-8', errors = 'ignore')
+    # site_limpo = unidecode(site_limpo)
+    site_limpo = normalize('NFKD', site_limpo).encode('utf-8', 'ignore').decode('ascii', 'ignore')
+    site_limpo = site_limpo.replace("             <span class=\"dateHidden\">", "") #Necessário para remover voos sem datas.
+    site_limpo = site_limpo.replace("UbI", "Uba") #Necessário para remover voos sem datas.
+    site_limpo = site_limpo.replace("UbAA", "Uba") #Necessário para remover voos sem datas.
+    site_limpo = site_limpo.replace("ValArio", "Valerio") #Necessário para remover voos sem datas.
+    #Aqui coleta-se os dados.
+    lista_id_voo = re.findall("href=\"/flight/([0-9]+)",site_limpo)
     lista_data = re.findall("<td class=\"dateString\" valign=\"top\">[\r\n ]+<div>[\r\n ]+(\d\d\/\d\d\/\d\d\d\d)",site_limpo)
-    lista_voo = [eval(x) for x in re.findall("(\d*\.?\d+\.\d+).km[\r\n][ ]+<\/td>[\r\n][ ]+<td class=\"OLC", site_limpo)]
-    df_voos_data = pd.DataFrame(list(zip(lista_data, lista_voo)),columns =['Data', 'kmOLC'])
-    # print(df_voos_data)
+    lista_kmOLC_voo = [eval(x) for x in re.findall("(\d*\.?\d+\.\d+).km[\r\n][ ]+<\/td>[\r\n][ ]+<td class=\"OLC", site_limpo)]
+    lista_duracao = re.findall("[\s]+(\d:\d\d)[\s]+<\/td>[\s]+<td class=\"distance\">",site_limpo)
+    lista_pontuacaoOLC = [eval(x) for x in re.findall("<td class=\"OLCScore\" nowrap=\"\">[\r\n ]+(\d*\.?\d+\.\d+)",site_limpo)]
+    lista_local_voo = re.findall("takeoffTip\.hide\(\)\">[\r\n ]+([A-Z][a-zA-Z ]+)",site_limpo)
+    #Monta o dataframe
+    df_voos_data = pd.DataFrame(list(zip(lista_id_voo,
+                                         lista_data, 
+                                         lista_kmOLC_voo,
+                                         lista_pontuacaoOLC,
+                                         lista_local_voo,
+                                         lista_duracao)),columns = ['ID_voo','Data', 'kmOLC','Pontuacao_OLC','local_voo','duracao'])
+    #Completa as colunas extras
+    df_voos_data['Piloto'] = piloto
+    df_voos_data['Categoria'] = categoria
+    df_voos_data['Vela'] = vela
+    df_voos_data['Data_Coleta'] = dt.today().date()
+    df_voos_data['Voo_data_valido'] = True
+    df_voos_data['Voo_rampa_valida'] = True
+    # Cria uma conexão com o banco de dados pra validar a rampa.
+    conn = sqlite3.connect('base_de_dados.db')
+    # Cria um cursor para executar comandos SQL
+    cursor = conn.cursor()
+    # Executa a consulta SELECT com uma cláusula WHERE para verificar se a rampa existe
+    for index, row in df_voos_data.iterrows():
+        rampa = row['local_voo']
+        cursor.execute("SELECT * FROM rampas_validas WHERE nome = ?", (rampa.rstrip(),))
+        resultados = cursor.fetchall()
+        # Verifica se a consulta retornou alguma linha
+        if len(resultados) > 0:
+            df_voos_data.at[index, 'Voo_rampa_valida'] = True
+        else:
+            df_voos_data.at[index, 'Voo_rampa_valida'] = False
+            
+    
+    # df_voos_data.to_sql('tabela_voos', conn, if_exists='append', index=False)
+    
+    #print(df_voos_data)
     #Aqui já tem os dados completos. Vamos agora dropar os voos acima da data limite.
     for index, row in df_voos_data.iterrows():
         data_voo = dt.strptime(row['Data'], "%d/%m/%Y")
         # data_limite = dt.strptime("01/01/23", "%d/%m/%y")
         if data_limite > data_voo:           
-            df_voos_data.drop(index, inplace=True)
-     #organiza os voos pela maior distancia
-    df_voos_data = df_voos_data.sort_values(by=['kmOLC'], ascending=False, ignore_index=True)
+            # df_voos_data.drop(index, inplace=True)
+            df_voos_data.at[index, 'Voo_data_valido'] = False
+    #organiza os voos pela maior distancia
+    df_voos_data = df_voos_data.sort_values(by=['Pontuacao_OLC'], ascending=False, ignore_index=True)
+    voos_validos = pd.concat([voos_validos, df_voos_data])
     #   Montar a apuração com os voos por piloto, na estrutura da planilha.
     #               'PILOTO':piloto ,
     #               'PARAPENTE': df_pilotos_ids.loc[i].at['Vela'],
@@ -56,7 +107,13 @@ for i in range(len(df_pilotos_ids)):
             df_apuracao.iloc[index_da_vez,i+3] = 0
 
 #organiza por quem ganhou e vamos embora.
+voos_validos = voos_validos.sort_values(by=['Pontuacao_OLC'], ascending=False, ignore_index=True)
+voos_validos.reset_index(drop=True)
 df_apuracao = df_apuracao.sort_values(by=['Distancia OLC KM'], ascending=False, ignore_index=True)
 df_apuracao.to_csv('apuracao.csv', sep=';') 
+voos_validos.to_csv('voos_validos.csv', sep=';')
+voos_validos.to_sql('tabela_voos', conn, if_exists='append', index=False)
+conn.commit()
+conn.close()
 print('FIM')
 
---
+++ cria_tabela_rampas.py
@@ -0,0 +1,61 @@
+import sqlite3
+import pandas as pd
+
+# Cria uma conexão com o banco de dados
+conn = sqlite3.connect('base_de_dados.db')
+
+# Cria um cursor para executar comandos SQL
+cursor = conn.cursor()
+
+# Define a estrutura da tabela
+cursor.execute('''CREATE TABLE IF NOT EXISTS rampas_validas (
+                    nome TEXT PRIMARY KEY,
+                    latitude REAL,
+                    longitude REAL
+                    )''')
+
+conn.commit()
+
+conn.execute('''CREATE TABLE IF NOT EXISTS tabela_voos
+             (ID_voo INT PRIMARY KEY NOT NULL,
+              Data TEXT NOT NULL,
+              kmOLC REAL NOT NULL,
+              Pontuacao_OLC REAL NOT NULL,
+              local_voo TEXT NOT NULL,
+              duracao TEXT NOT NULL,
+              Piloto TEXT NOT NULL,
+              Categoria TEXT NOT NULL,
+              Vela TEXT NOT NULL,
+              Data_Coleta TEXT NOT NULL,
+              Voo_data_valido BOOLEAN NOT NULL,
+              Voo_rampa_valida BOOLEAN NOT NULL);''')
+
+# Salva as alterações no banco de dados
+conn.commit()
+
+# Lê o arquivo CSV e cria um DataFrame com os dados
+df = pd.read_csv('Rampas/Rampas_Filtradas.csv', sep=';')
+
+# Insere os dados do DataFrame na tabela SQLite
+df.to_sql('rampas_validas', conn, if_exists='replace', index=False)
+
+
+# Solicita ao usuário o nome da rampa a ser verificada
+rampa = 'Vila Valerio '
+
+# Executa a consulta SELECT com uma cláusula WHERE para verificar se a rampa existe
+cursor.execute("SELECT * FROM rampas_validas WHERE nome = ?", (rampa.rstrip(),))
+
+# Recupera os resultados da consulta
+resultados = cursor.fetchall()
+
+# Verifica se a consulta retornou alguma linha
+if len(resultados) > 0:
+    print('A rampa', rampa, 'existe na tabela.')
+else:
+    print('A rampa', rampa, 'não existe na tabela.')
+
+# Fecha a conexão com o banco de dados
+
+# Fecha a conexão com o banco de dados
+conn.close()
